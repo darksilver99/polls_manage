55d53451d5aa8e1d43765b11dfcfc8d04c51ef0d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-02 10:53:22 +0000

    Updating to latest FlutterFlow output.

--- lib/poll_view/step_view/step_view_widget.dart
+++ lib/poll_view/step_view/step_view_widget.dart
@@ -44,9 +44,9 @@ class _StepViewWidgetState extends State<StepViewWidget> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
+      padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 16.0),
       child: Column(
-        mainAxisSize: MainAxisSize.max,
+        mainAxisSize: MainAxisSize.min,
         children: [
           Padding(
             padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 8.0),
