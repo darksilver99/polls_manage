59bfda4d26e710d4853f2f098868592ba05a115c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-09 11:23:46 +0000

    Updating to latest FlutterFlow output.

--- lib/poll_view/poll_summary_page/poll_summary_page_widget.dart
+++ lib/poll_view/poll_summary_page/poll_summary_page_widget.dart
@@ -1,4 +1,5 @@
 import '/backend/backend.dart';
+import '/backend/schema/structs/index.dart';
 import '/components/back_button_view_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
