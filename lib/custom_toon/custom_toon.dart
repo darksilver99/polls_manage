08802eb8c555106838c33eda13fc2d21a45b9f61
darksilver99 <darksilver99@hotmail.com>
2024-12-09 14:29:32 +0700

    a

--- lib/custom_toon/custom_toon.dart
+++ lib/custom_toon/custom_toon.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:widgets_to_image/widgets_to_image.dart';
+
 
 Future<String?> getSaveAndGetImagePath(WidgetsToImageController controller) async {
   final bytes = await controller.capture();
@@ -15,4 +17,28 @@ Future<String?> getSaveAndGetImagePath(WidgetsToImageController controller) asyn
   await File(tempPath).writeAsBytes(base64Decode(base64Image));
 
   return tempPath;
+}
+
+
+Future<List<String>> getHeader(DocumentReference pollReference) async {
+
+  try {
+    var rs = await pollReference.get();
+    var data = rs.data() as Map<String, dynamic>?;
+    if (data == null || !data.containsKey('question_list')) {
+      return [];
+    }
+
+    var questionList = data['question_list'] as List<dynamic>;
+    var topics = questionList
+        .where((question) => question is Map && question.containsKey('topic'))
+        .map((question) => question['topic'] as String)
+        .toList();
+
+    return topics;
+  } catch (e) {
+    print("Error: $e");
+    return [];
+  }
+
 }
